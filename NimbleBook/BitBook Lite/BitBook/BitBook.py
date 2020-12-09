874b012ce1b1929387f62bbb1d8cdf2df8e79f2b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-08 20:51:36 -0800

    BitBook.py
    The BitBook script for NimbleBook.

---
+++ NimbleBook/BitBook Lite/BitBook/BitBook.py
@@ -0,0 +1,24 @@
+# Start of script
+# Import section
+import random
+# Currency
+displayBux = int(20) # Reference to the video ad reward
+displayCoins = int(1230) # Random assortment of numbers for the 1-3 coin gains
+displayGT = str("0/5") # Reference to Charlie and the chocolate factory
+profileName1 = str("Anonymous")
+likeCountNew = int(random.randint(0,46))
+# Header
+print ("\t\tBitBook\t\t[x]") # Centered: BitBook, close button at end
+# <HR> (find Python substitute)
+print (profileName1())
+print ("BitBook Lite is under construction. Help by contributing. It is meant to look similar to BitBook")
+print ("+ " + str(likeCountNew))
+quitIt = input("Quit BitBook")
+print ("The window should now be closed. If the program is still not closed, try pressing the close button. If this doesn't work, try ending the process/task with a process manager/task manager")
+"""
+File info
+File type: Python script file (*.py)
+File version: 1 (Tuesday, December 8th 2020 at 8:51 pm)
+Line count (including blank lines and compiler line): 25
+"""
+# End of script
